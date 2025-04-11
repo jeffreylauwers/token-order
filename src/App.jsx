ab8c10e15b0cd44fb1f2910d5fafd27539e66f74
Jeffrey Lauwers <jeffreylauwers@gmail.com>
2025-04-11 15:32:56 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import JSONTokenSorter from './components/JSONTokenSorter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <JSONTokenSorter />
+    </div>
+  );
+}
+
+export default App;
